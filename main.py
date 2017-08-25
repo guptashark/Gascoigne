e47b805fdf2de7b6adf93d3e64fa462e2d2f2b15
Aishwary Gupta <guptashark@gmail.com>
2017-08-25 19:19:57 -0400

    basic static template running

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 import webapp2
-from jinja2 import Template
+import jinja2
+import os
+
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir))
 
 class MainPage(webapp2.RequestHandler):
     def get(self):
-        self.response.headers['Content-Type'] = 'text/plain'
-        self.response.write('Hello World, this is Ash!')
+        #self.response.headers['Content-Type'] = 'text/plain'
+        #self.response.write('Hello World, this is Ash!')
+        t = jinja_env.get_template('main.html')
+        print self.response.write(t.render())
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
