afbc08358b3a16af5c1f447f395bb30dcd30ad8d
Aishwary Gupta <guptashark@gmail.com>
2017-08-27 13:37:57 -0400

    Basic template inheritance works

--- testhandler.py
+++ testhandler.py
@@ -3,7 +3,20 @@ from base_headers import jinja_env
 
 class TestHandler(webapp2.RequestHandler):
     def get(self): 
-        #self.response.headers['Content-Type'] = 'text/plain'
-        #self.response.write('Hello World, this is Ash on a testpage!')
+        """ Writing without specifying content type... """ 
+        """ 
+        self.response.write('Hello World')
+        """ 
+       
+        """ Writing with content type specification as html """ 
+        """ 
+        self.response.headers['Content-Type'] = 'text/html'
+        self.response.write('Hello World, this is Ash on a testpage!')
+        """ 
+
+        """ Writing by using the templating engine to render a template """ 
+        
         t = jinja_env.get_template('test.html')
         self.response.write(t.render())
+        
+        
