cca50acb3c6f65ddf0ee93d193ed6e91e808f185
Aishwary Gupta <guptashark@gmail.com>
2017-08-28 23:06:25 -0400

    Question checking now possible with quiz page

--- quiz.py
+++ quiz.py
@@ -1,11 +1,28 @@
 import webapp2
 from base_headers import jinja_env
 
-basket = ['apple', 'banana', 'orange', 'pear']
+test_integers = [
+        ('3 - 5 = ', "ans_01", "-2"),
+        ('4 + 7 = ', "ans_02", "11"),
+        ('12 - 13 = ', "ans_03", "-1")]
+
 
 class Quiz(webapp2.RequestHandler):
     def get(self):
         t = jinja_env.get_template('quiz.html')
-        self.response.write(t.render(basket = basket))
+        self.response.write(t.render(test = test_integers))
+
+    def post(self):
+        num_questions = len(test_integers)
+        num_correct = 0
+        for question in test_integers:
+            student_ans = self.request.POST[question[1]]
+            if(student_ans == question[2]):
+                num_correct = num_correct + 1
+        if(num_correct / float(num_questions) > 0.5):
+            self.response.write("You passed the test!")
+        else:
+            self.response.write("You failed the test. :( ")
+            
         
 
