d9e6ed42bb94875ee1a62f53c2073ec6e3114c01
Aishwary Gupta <guptashark@gmail.com>
2017-08-28 22:30:47 -0400

    very basic for loop working

--- quiz.py
+++ quiz.py
@@ -1,9 +1,11 @@
 import webapp2
 from base_headers import jinja_env
 
+basket = ['apple', 'banana', 'orange', 'pear']
+
 class Quiz(webapp2.RequestHandler):
     def get(self):
         t = jinja_env.get_template('quiz.html')
-        self.response.write(t.render())
+        self.response.write(t.render(basket = basket))
         
 
